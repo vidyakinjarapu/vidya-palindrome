968255c62965c51e519f9c6ee40f1a020f97c844
vidya kinjarapu <vidya5594@gmail.com>
2021-02-12 11:33:31 +0530

    adding export module code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+module.exports = Phrase
+
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
@@ -9,31 +12,13 @@ String.prototype.reverse = function() {
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
 
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
-  };
+    return this.content.toLowerCase();
+  }
+
 
   this.palindrome = function palindrome() {
    return this.processedContent() === this.processedContent().reverse();
  }
 }
-
-//to apply palindrome directly on a String
-String.prototype.palindrome = function() {
-  return new Phrase(this).palindrome();
-}
-
-// function TranslatedPhrase(content, translation) {
-//   this.content = content;
-//   this.translation = translation;
-//
-//   this.processedContent = function processedContent() {
-//     return this.processor(this.translation);;
-//   };
-//   // TranslatedPhrase.prototype = new Phrase();
-// }
-// TranslatedPhrase.prototype = new Phrase();
